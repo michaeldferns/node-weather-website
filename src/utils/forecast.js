c52772d599d7cb41252d20a61db373c668647ef1
Michael Ferns <michaeldferns@gmail.com>
2019-07-28 20:00:37 -0400

    changes to weather forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find the location.')
         } else {
-            callback (undefined, `It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+            callback (undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
         }
     })
 }
